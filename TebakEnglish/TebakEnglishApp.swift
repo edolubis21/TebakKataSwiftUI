c2196daacaa19e0e372389f09cfa712d8454dfa1
edolubis-ta <edo.lubis@tentanganak.id>
2024-03-04 20:24:43 +0700

    Initial Commit

---
+++ TebakEnglish/TebakEnglishApp.swift
@@ -0,0 +1,17 @@
+//
+//  TebakEnglishApp.swift
+//  TebakEnglish
+//
+//  Created by edo lubis on 04/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TebakEnglishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
